58eb020e66ac87ab44fa6962c7687f363c8734cb
sbikcs <nataliebri@icloud.com>
2026-07-10 23:19:35 -0400

    BookingView buttons added

---
+++ HotelSeneca/src/main/java/ca/seneca/hotel/controller/admin/ReservationDetailsController.java
@@ -0,0 +1,17 @@
+package ca.seneca.hotel.controller.admin;
+
+import javafx.event.ActionEvent;
+
+public class ReservationDetailsController {
+    public void handleCheckAvailability(ActionEvent actionEvent) {
+    }
+
+    public void handleDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleUpdate(ActionEvent actionEvent) {
+    }
+}
