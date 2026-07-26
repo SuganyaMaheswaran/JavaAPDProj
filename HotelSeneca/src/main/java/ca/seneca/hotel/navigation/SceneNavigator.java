e6d7ebb83a107286b9af42340ee055d4f9708a09
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-07-25 21:04:19 -0400

    Create SceneNavigator.java

---
+++ HotelSeneca/src/main/java/ca/seneca/hotel/navigation/SceneNavigator.java
@@ -0,0 +1,51 @@
+package ca.seneca.hotel.navigation;
+
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.function.Consumer;
+
+/**
+ * STREAMING_CHUNK:Initializing SceneNavigator state...
+ * Manages view switching and modal windows for the Hotel System.
+ */
+public class SceneNavigator {
+    private final Stage primaryStage;
+    private Object bookingDraft; // Carrier for booking state
+
+    public SceneNavigator(Stage stage) {
+        this.primaryStage = stage;
+    }
+
+    public void goTo(String fxml) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/" + fxml));
+        primaryStage.setScene(new Scene(root));
+    }
+
+    /**
+     * STREAMING_CHUNK:Defining initialization logic for controllers...
+     */
+    public <T> void goTo(String fxml, Consumer<T> init) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/" + fxml));
+        Parent root = loader.load();
+        init.accept(loader.getController());
+        primaryStage.setScene(new Scene(root));
+    }
+
+    public void openModal(String fxml, String title) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/" + fxml));
+        Stage modal = new Stage();
+        modal.initModality(Modality.APPLICATION_MODAL);
+        modal.setTitle(title);
+        modal.setScene(new Scene(root));
+        modal.showAndWait();
+    }
+
+    public Object getDraft() { return bookingDraft; }
+    public void resetDraft() { this.bookingDraft = null; }
+}
