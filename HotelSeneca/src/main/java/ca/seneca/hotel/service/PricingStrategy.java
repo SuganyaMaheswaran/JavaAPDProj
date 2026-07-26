4341a99042bbeda6466cc274960a42109ec15e81
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-07-25 21:04:26 -0400

    Create PricingStrategy.java

---
+++ HotelSeneca/src/main/java/ca/seneca/hotel/service/PricingStrategy.java
@@ -0,0 +1,31 @@
+package ca.seneca.hotel.service;
+
+import ca.seneca.hotel.model.Room;
+import java.time.LocalDate;
+import java.time.DayOfWeek;
+
+/**
+ * STREAMING_CHUNK:Implementing Strategy pattern for pricing...
+ */
+public interface PricingStrategy {
+    double calculateNightlyRate(Room room, LocalDate date);
+}
+
+class StandardPricingStrategy implements PricingStrategy {
+    @Override
+    public double calculateNightlyRate(Room room, LocalDate date) {
+        return room.getBasePrice();
+    }
+}
+
+class WeekendPricingStrategy implements PricingStrategy {
+    private static final double MULTIPLIER = 1.2;
+    @Override
+    public double calculateNightlyRate(Room room, LocalDate date) {
+        DayOfWeek dow = date.getDayOfWeek();
+        if (dow == DayOfWeek.SATURDAY || dow == DayOfWeek.SUNDAY) {
+            return room.getBasePrice() * MULTIPLIER;
+        }
+        return room.getBasePrice();
+    }
+}
