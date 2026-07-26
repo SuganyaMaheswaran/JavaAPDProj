fd1749b55b64e799c09492b9c7789ad438621fc7
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-07-25 17:51:36 -0700

    Delete HotelKioskApp directory
    No longer needed

--- HotelKioskApp/src/Launcher.java
+++
@@ -1,8 +0,0 @@
-public class Launcher {
-    public static void main(String[] args) {
-        // Mutes the "unnamed module" system logging output
-        System.setProperty("javafx.verbose", "false"); 
-        
-        MainApplication.main(args);
-    }
-}
