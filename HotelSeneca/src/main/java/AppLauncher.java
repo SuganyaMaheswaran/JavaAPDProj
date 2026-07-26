bd1d5b9c1248d04240fcd3c92787e63520eecc9a
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-07-26 11:41:24 -0400

    Create AppLauncher.java

---
+++ HotelSeneca/src/main/java/AppLauncher.java
@@ -0,0 +1,11 @@
+import ca.seneca.hotel.main.Main;
+
+/**
+ * Non-Application starter class to bypass JavaFX runtime component checks.
+ */
+public class AppLauncher {
+    public static void main(String[] args) {
+        // Calls the main method of your actual JavaFX Main class
+        Main.main(args);
+    }
+}
