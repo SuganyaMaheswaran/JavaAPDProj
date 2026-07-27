b7f8df188ac25cd985f442de5a207a2436b2a173
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-07-27 08:17:24 -0400

    Update SceneNavigator.java

--- HotelSeneca/src/main/java/ca/seneca/hotel/navigation/SceneNavigator.java
+++ HotelSeneca/src/main/java/ca/seneca/hotel/navigation/SceneNavigator.java
@@ -15,6 +15,9 @@ import java.util.function.Consumer;
  * Manages view switching and modal windows for the Hotel System.
  */
 public class SceneNavigator {
+    private static final double SCREEN_WIDTH = 1000;
+    private static final double SCREEN_HEIGHT = 700;
+
     private final Stage primaryStage;
     private Object bookingDraft; // Carrier for booking state
 
@@ -24,7 +27,7 @@ public class SceneNavigator {
 
     public void goTo(String fxml) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/" + fxml));
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
     }
 
     /**
@@ -33,7 +36,7 @@ public class SceneNavigator {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/" + fxml));
         Parent root = loader.load();
         init.accept(loader.getController());
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
     }
 
     public void openModal(String fxml, String title) throws IOException {
@@ -41,7 +44,7 @@ public class SceneNavigator {
         Stage modal = new Stage();
         modal.initModality(Modality.APPLICATION_MODAL);
         modal.setTitle(title);
-        modal.setScene(new Scene(root));
+        modal.setScene(new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT));
         modal.showAndWait();
     }
 
