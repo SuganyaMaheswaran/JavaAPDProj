cfed79fad2c12ffdb9f326e6e37eb794c0d7c298
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-07-27 08:16:45 -0400

    Update KioskWelcomeController.java

--- HotelSeneca/src/main/java/ca/seneca/hotel/controller/kiosk/KioskWelcomeController.java
+++ HotelSeneca/src/main/java/ca/seneca/hotel/controller/kiosk/KioskWelcomeController.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class KioskWelcomeController {
+public class KioskWelcomeController extends KioskInfoController {
 
     @FXML
     private void handleStartBooking(ActionEvent event) {
