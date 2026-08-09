d35938d063f59c677d911e5cd3560b8211076919
Suganya Maheswaran <43247716+SuganyaMaheswaran@users.noreply.github.com>
2026-08-09 14:48:14 -0400

    Updated icons to use material ui icons

--- HotelSeneca/src/main/java/ca/seneca/hotel/controller/kiosk/KioskWelcomeController.java
+++ HotelSeneca/src/main/java/ca/seneca/hotel/controller/kiosk/KioskWelcomeController.java
@@ -9,4 +9,9 @@ public class KioskWelcomeController extends KioskInfoController {
     private void handleStartBooking(ActionEvent event) {
         switchScene(event, "/view/kiosk/kiosk_guests_input_view.fxml", "Hotel Reservation - Step 1: Guests");
     }
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+        switchScene(event, "/view/WelcomeView.fxml", "Hotel Reservation System - Self-Service Kiosk");
+    }
 }
